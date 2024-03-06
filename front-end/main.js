b06636c284c785e773d4073a5da37ee931d61e36
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-06 20:00:21 +0100

    add method axios post api get list

--- front-end/main.js
+++ front-end/main.js
@@ -3,7 +3,44 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-        
+        newTask:{
+          text:'',
+          done:false
+        },
+
+        todoList:[],
     }
-  }
+  },
+
+
+  methods: {
+
+    // CHIAMA TASK DAL JSON
+    getTasks(){
+      
+      axios.post('../back-end/api/get-list.php',
+      {
+        headers:{'Content-Type': 'multipart/form-data'}
+      }).then((response) => {
+        this.todoList = response.data;
+      });
+
+    },
+
+    // MANDA TASK
+    addTask(){
+
+
+
+    },
+
+
+  },
+
+  mounted() {
+
+    this.getTasks();
+
+  },
+
 }).mount('#app')
